a9e9c681a18ac00f329e1e197d4cdcc4b347ed03
lekha005 <41885177+lekha005@users.noreply.github.com>
2018-07-30 19:49:59 +0530

    Create Next.java

---
+++ Next.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Next
+{
+    public static void main(String hj[])
+    {
+        Scanner in=new Scanner(System.in);
+        int a=in.nextInt();
+       System.out.print(a+1);
+}
+}
+
+
+
