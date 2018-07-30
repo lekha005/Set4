813f34a4d4eb1ecaedf76c018e41c45ff83cc2cf
lekha005 <41885177+lekha005@users.noreply.github.com>
2018-07-30 12:20:00 +0000

    Create Stlen.java

---
+++ Stlen.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Stlen
+{
+    public static void main(String hj[])
+    {
+        Scanner in=new Scanner(System.in);
+         String a=in.nextLine().replace(" ","");
+         System.out.print(a.length());
+}
+}
+
