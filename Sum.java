3309b4307bd8d2615804716993f3c02c2f34ff9f
lekha005 <41885177+lekha005@users.noreply.github.com>
2018-07-30 20:43:11 +0530

    Create Sum.java

---
+++ Sum.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Sum
+{
+    public static void main(String hj[])
+    {
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+        int m=in.nextInt();
+       System.out.print(n+m);
+}
+}
+
+
+
