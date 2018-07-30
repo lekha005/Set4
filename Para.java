dc902741444a9515e01d7b2d819b0a8bed8b2661
lekha005 <41885177+lekha005@users.noreply.github.com>
2018-07-30 12:33:09 +0000

    Create Para.java

---
+++ Para.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Para
+{
+    public static void main(String hj[])
+    {
+        Scanner in=new Scanner(System.in);
+         String[] a=in.nextLine().split("//.");
+         System.out.print(a.length);
+}
+}
+
+
