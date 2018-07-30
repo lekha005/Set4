092dad6ce21cd4f05b8a4932b727785cb881e8c6
lekha005 <41885177+lekha005@users.noreply.github.com>
2018-07-30 20:51:18 +0530

    Create 2mul.java

---
+++ 2mul.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class 2mul
+{
+    public static void main(String hj[])
+    {
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+        if(n%2==0)
+       System.out.print("yes");
+       else
+       System.out.print("no");
+}
+}
+
+
+
