ba7a70382f67c3de92797b12e58a0de496d0dd13
lekha005 <41885177+lekha005@users.noreply.github.com>
2018-07-30 12:26:12 +0000

    Create Stli.java

---
+++ Stli.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Stli
+{
+    public static void main(String hj[])
+    {
+        Scanner in=new Scanner(System.in);
+         String[] a=in.nextLine().split(" ");
+         System.out.print(a.length-1);
+}
+}
+
+
