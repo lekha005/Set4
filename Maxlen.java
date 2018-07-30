0bd0ec04b45ac90fa79151391e4b71082f37f802
lekha005 <41885177+lekha005@users.noreply.github.com>
2018-07-30 13:19:39 +0000

    Create Maxlen.java

---
+++ Maxlen.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Maxlen
+{
+    public static void main(String hj[])
+    {
+        Scanner in=new Scanner(System.in);
+        String a=in.next();
+        String b=in.next();
+        if(a.length()>=b.length())
+        {
+        System.out.println(a);
+        }
+        else
+        System.out.print(b);
+}
+}
+
+
