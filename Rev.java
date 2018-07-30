ec7e30103e2ae5317bea322d43f01403c17a6d76
lekha005 <41885177+lekha005@users.noreply.github.com>
2018-07-30 12:46:16 +0000

    Create Rev.java

---
+++ Rev.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Rev
+{
+    public static void main(String hj[])
+    {
+        Scanner in=new Scanner(System.in);
+         String[] a=in.nextLine().split(" ");
+         int d=0;
+         for(int i=a.length-1;i>=0;i--)
+         {
+            System.out.print(a[i]+" ");
+         }
+}
+}
+
+
