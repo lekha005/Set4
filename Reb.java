c6455f7ffc0a516459452e43de26c97f64dcc13c
lekha005 <41885177+lekha005@users.noreply.github.com>
2018-07-30 13:15:51 +0000

    Create Reb.java

---
+++ Reb.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Reb
+{
+    public static void main(String hj[])
+    {
+        Scanner in=new Scanner(System.in);
+        String a=in.next();
+        int n=in.nextInt();
+        for(int i=0;i<n;i++)
+        {
+        System.out.println(a+" ");
+        }
+}
+}
+
+
