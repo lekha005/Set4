265ea190de929bad0997878a9ae18bc7b0eca140
lekha005 <41885177+lekha005@users.noreply.github.com>
2018-07-30 13:21:32 +0000

    Create Con.java

---
+++ Con.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Con
+{
+    public static void main(String hj[])
+    {
+        Scanner in=new Scanner(System.in);
+        String a=in.next();
+        String b=in.next();
+        String ab=a+b;
+        System.out.println(ab);
+        
+}
+}
+
+
